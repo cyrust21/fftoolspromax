af3e78f0aee65bcdb467da0a737ef5fbf3ea888c
Cyrust Mods <45724743+cyrust21@users.noreply.github.com>
2023-04-01 00:50:43 +0700

    Create Test.php

---
+++ Test.php
@@ -0,0 +1,5 @@
+function generateRandomString($length = 10) {
+    return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+}
+
+echo  generateRandomString();
